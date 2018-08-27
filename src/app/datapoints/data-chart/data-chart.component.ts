8afd3c268d5d23c9a0867b4a7dbfdd3834b85a64
Carole <cjpillette@hotmail.com>
2018-08-27 13:52:51 +0200

    Implemented combo line bar chart with chart.js

--- src/app/datapoints/data-chart/data-chart.component.ts
+++ src/app/datapoints/data-chart/data-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { DataPointsService } from '../data-points.service';
 import * as Chart from 'chart.js';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-data-chart',
@@ -16,9 +17,6 @@ export class DataChartComponent implements OnInit, AfterViewInit {
   checkType: string;
   isDataAvailable = false;
   unit: string;
-  chart: any;
-  ctx: any;
-  canvas: any;
 
   lineChartOptions = {
     scales: {
@@ -84,24 +82,115 @@ export class DataChartComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     const canvas: any = document.getElementById('myChart');
     const ctx = canvas.getContext('2d');
-      const myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['New', 'In Progress', 'On Hold'],
-            datasets: [{
-                label: '# of Votes',
-                data: [1, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)'
+    this.dataPointsService.readPointsOf(this.collection).subscribe(
+      val => {
+        this.valuePoints = val.map(a => a.value);
+        const datePoints = val.map(a => a.date);
+        const upperLimit = val.map(a => a.upperLimit);
+        const lowerLimit = val.map(a => a.lowerLimit);
+        const unitArray = val.map(a => a.unit);
+        this.unit = Array.from(new Set(unitArray)).join(' ');
+
+        const dates = [];
+        datePoints.forEach( res => {
+            dates.push(moment(res).locale('fr').format('YYYY-MM-DD'));
+          });
+
+        const myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: dates,
+            datasets: [
+              {
+                type: 'bar',
+                label: 'Traitements',
+                data: [100, 0,0,0,0,100,0,0,0,100,0,0,0,0,0,0,0,0,0,100,0,0,0,0],
+                backgroundColor: 'rgba(0, 0, 0, 1)',
+                borderWidth: 0,
+              },
+              {
+                type: 'line',
+                label: this.collection,
+                data: this.valuePoints,
+                xAxisID: 'value-time',
+                yAxisID: 'value-amount',
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(75, 00, 150,1)',
+                borderWidth: 2,
+                pointRadius: 1,
+              },
+              {
+                type: 'line',
+                label: 'Upper Limit',
+                data: upperLimit,
+                xAxisID: 'value-time',
+                yAxisID: 'value-amount',
+                borderColor: [
+                'rgba(255, 99, 132, 1)'
                 ],
+                backgroundColor: 'transparent',
+                pointBorderColor: 'transparent',
                 borderWidth: 1
-            }]
-        },
-        options: {
-          responsive: false,
-        }
+              },
+              {
+                type: 'line',
+                label: 'Lower Limit',
+                data: lowerLimit,
+                xAxisID: 'value-time',
+                yAxisID: 'value-amount',
+                borderColor: [
+                'rgba(255, 66, 66, 1)'
+                ],
+                backgroundColor: 'transparent',
+                pointBorderColor: 'transparent',
+                borderWidth: 1
+              }
+          ]
+          },
+          options: {
+            legend: {
+              display: false
+              },
+            scales: {
+              xAxes: [
+                {
+                  display: false,
+                  barThickness: 2
+                },
+                {
+                  id: 'value-time',
+                  type: 'time',
+                  distribution: 'linear',
+                }
+              ],
+              yAxes: [
+                {
+                  display: false,
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'traitement'
+                    }
+                },
+                {
+                  id: 'value-amount',
+                  display: true,
+                  ticks: {
+                    beginAtZero: false,
+                                max: 7.5
+                  }
+                },
+              ]
+            },
+            tooltips: {
+              callbacks: {
+                title: function(tooltipItem, data) {
+                  const yyyymmdd = tooltipItem[0].xLabel;
+                  const title = moment(yyyymmdd).locale('fr').format('MMM YYYY');
+                  return title;
+              }
+            }
+          }
+        });
       });
     }
 
